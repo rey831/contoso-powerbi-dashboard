71072d6b4ad7ea0c942bcf22ffc834f1ea65e910
rey831 <reynardw99@gmail.com>
2025-04-17 21:19:10 +0200

    sql_queries_contoso.sql

--- sql_queries_contoso.sql
+++ sql_queries_contoso.sql
@@ -1,27 +1,81 @@
 
 -- FACT SALES QUERY
-SELECT *
-FROM FactSales
-WHERE Region IN ('Europe', 'Asia', 'North America')
-AND [Date] >= '2022-01-01';
+Create VIEW add_date_city_add AS
+SELECT 
+	dbo.FactSales.SalesKey AS SalesKey,
+    DATEADD(year, 14, DateKey) AS NewDate,
+	dbo.FactSales.channelKey AS ChannelKey,
+	dbo.FactSales.StoreKey AS StoreKey,
+	dbo.FactSales.ProductKey AS ProductKey, 
+	dbo.FactSales.UnitCost AS UnitCost,
+	dbo.FactSales.UnitPrice AS UnitPrice,
+	dbo.FactSales.SalesQuantity AS SalesQuantity,
+	dbo.FactSales.ReturnQuantity AS ReturnQuantity,
+	dbo.FactSales.ReturnAmount AS ReturnAmount, 
+	dbo.FactSales.DiscountQuantity AS DiscountQTY,
+	dbo.FactSales.DiscountAmount AS DiscountAmount, 
+	dbo.FactSales.TotalCost AS TotalCost, 
+	dbo.FactSales.SalesAmount AS SalesAmount
+FROM dbo.FactSales
+INNER JOIN
+	dbo.DimStore ON dbo.Dimstore.StoreKey = dbo.FactSales.StoreKey
+INNER JOIN
+	dbo.DimGeography ON dbo.Dimstore.Geographykey = dbo.DimGeography.Geographykey
+WHERE DateKey BETWEEN '2007-01-01' AND '2009-12-31'
+AND dbo.dimGeography.ContinentName IN ('Europe', 'North America', 'Asia');
 
--- DIMENSION STORE QUERY
-SELECT StoreKey, StoreName, City, StateProvinceName, Region
-FROM DimStore
-WHERE Status = 'Active';
+-- Dimension Store
+CREATE VIEW dim_storegeo AS
+SELECT
+    dbo.DimGeography.GeographyKey AS GeographyKey,
+    dbo.DimStore.StoreKey AS StoreKey ,
+	dbo.DimStore.StoreName AS StoreName, 
+	dbo.DimGeography.RegionCountryName AS CountryName, 
+	dbo.DimGeography.ContinentName AS ContinentName
+FROM
+    dbo.DimGeography
+INNER JOIN
+    dbo.DimStore
+ON 
+    dbo.DimGeography.GeographyKey = dbo.DimStore.GeographyKey
+WHERE dbo.dimGeography.ContinentName IN ('Europe', 'North America', 'Asia');
 
--- DIMENSION GEOGRAPHY QUERY
-SELECT *
-FROM DimGeography;
+-- Dimension Geography
+select * from dbo.DimGeography
+where ContinentName = 'Europe' OR ContinentName = 'North America' OR ContinentName = 'Asia';
 
--- DIMENSION SALES TERRITORY QUERY
-SELECT SalesTerritoryKey, SalesTerritoryRegion, SalesTerritoryCountry
-FROM DimSalesTerritory;
+-- Dimension Sales Territory
+select * from dbo.DimSalesTerritory
+Where SalesTerritoryGroup = 'Europe';
 
--- DIMENSION PRODUCT CATEGORY QUERY
-SELECT ProductCategoryKey, ProductCategoryName
-FROM DimProductCategory;
+-- Dimension Product Category
+select * from dbo.DimProductCategory;
+select * from dbo.DimProductSubcategory;
 
--- DIMENSION FACT ONLINE SALES QUERY
-SELECT *
-FROM FactOnlineSales;
+-- Dimension Fact Online Sales
+Create VIEW add_online_sales_1 AS
+SELECT 
+	dbo.FactOnlineSales.OnlineSalesKey AS SalesKey,
+    DATEADD(year, 14, DateKey) AS NewDate,
+	dbo.FactOnlineSales.StoreKey AS StoreKey,
+	dbo.FactOnlineSales.ProductKey AS ProductKey, 
+	dbo.FactOnlineSales.CustomerKey AS CustomerKey,
+	dbo.FactOnlineSales.SalesOrderNumber AS SalesOrderNumber,
+	dbo.FactOnlineSales.SalesQuantity AS SalesQuantity,
+	dbo.FactOnlineSales.SalesAmount AS SalesAmount,
+	dbo.FactOnlineSales.ReturnQuantity AS ReturnQty,
+	dbo.FactOnlineSales.ReturnAmount AS ReturnAmount, 
+	dbo.FactOnlineSales.DiscountQuantity AS DiscountQTY,
+	dbo.FactOnlineSales.DiscountAmount AS DiscountAmount, 
+	dbo.FactOnlineSales.TotalCost AS TotalCost, 
+	dbo.FactOnlineSales.UnitCost AS UnitCost, 
+	dbo.FactOnlineSales.UnitPrice AS UnitPrice 
+FROM dbo.FactOnlineSales
+INNER JOIN
+	dbo.DimStore ON dbo.Dimstore.StoreKey = dbo.FactOnlineSales.StoreKey
+INNER JOIN
+	dbo.DimGeography ON dbo.Dimstore.Geographykey = dbo.DimGeography.Geographykey
+WHERE DateKey BETWEEN '2007-01-01' AND '2009-12-31'
+AND dbo.dimGeography.ContinentName IN ('Europe', 'North America', 'Asia');
+
+
